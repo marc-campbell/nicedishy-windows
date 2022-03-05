1fb80ba87d0d125c3595fa591b66e7b6ca313559
Marc Campbell <marc.e.campbell@gmail.com>
2022-03-05 11:39:43 -0800

    Work on CI

---
+++ deploy/ArtifactBuild.cmd
@@ -0,0 +1,7 @@
+@echo off
+pushd "%~dp0"
+powershell Compress-7Zip "Release" -ArchiveFileName "NiceDishyX86.zip" -Format Zip
+powershell Compress-7Zip "x64\Release" -ArchiveFileName "NiceDishyX64.zip" -Format Zip
+:exit
+popd
+@echo on
