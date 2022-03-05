a901d8e20518fe6437020815d875547bb26c378f
Marc Campbell <marc.e.campbell@gmail.com>
2022-03-05 12:22:31 -0800

    debug

--- deploy/ArtifactBuild.cmd
+++ deploy/ArtifactBuild.cmd
@@ -2,6 +2,8 @@
 pushd "%~dp0"
 powershell Compress-7Zip "Release" -ArchiveFileName "NiceDishyX86.zip" -Format Zip
 powershell Compress-7Zip "x64\Release" -ArchiveFileName "NiceDishyX64.zip" -Format Zip
+dir
+echo %cd%
 :exit
 popd
 @echo on
--- deploy/Build.cmd
+++ deploy/Build.cmd
@@ -5,6 +5,8 @@ if exist Release rd /s /q Release
 if exist x64 rd /s /q x64
 "%programfiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" /p:Configuration=Release /p:Platform=x64 ..\NiceDishy\NiceDishy.csproj
 "%programfiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" /p:Configuration=Release /p:Platform=x86 ..\NiceDishy\NiceDishy.csproj
+dir
+echo %cd%
 :exit
 popd
 @echo on
