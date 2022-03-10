7bd5288b4d2d0803d972c3ac77d4e8a508606cad
Marc Campbell <marc.e.campbell@gmail.com>
2022-03-09 21:16:51 -0800

    debug

--- deploy/ArtifactBuild.cmd
+++ deploy/ArtifactBuild.cmd
@@ -1,6 +1,10 @@
 @echo off
 pushd "%~dp0"
 powershell [IO.Path]::GetFullPath((join-path (Get-Location) "..\x86\Release\NiceDishy.exe"))
+echo "%cd%"
+dir
+dir D:\a\nicedishy-windows\nicedishy-windows
+dir D:\a\nicedishy-windows\nicedishy-windows\x86
 dir D:\a\nicedishy-windows\nicedishy-windows\x86\Release
 echo "Creating x86 zip"
 powershell Compress-7Zip -Path ([IO.Path]::GetFullPath((join-path (Get-Location) "..\x86\Release\NiceDishy.exe"))) -ArchiveFileName "NiceDishyX86.zip" -Format Zip
