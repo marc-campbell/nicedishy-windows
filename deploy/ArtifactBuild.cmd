942f31f2de2e8480b54c21317afa23bf545b6688
Marc Campbell <marc.e.campbell@gmail.com>
2022-03-09 21:21:42 -0800

    path

--- deploy/ArtifactBuild.cmd
+++ deploy/ArtifactBuild.cmd
@@ -1,15 +1,15 @@
 @echo off
 pushd "%~dp0"
-powershell [IO.Path]::GetFullPath((join-path (Get-Location) "..\x86\Release\NiceDishy.exe"))
+powershell [IO.Path]::GetFullPath((join-path (Get-Location) "..\NiceDishy\x86\Release\NiceDishy.exe"))
 echo "%cd%"
 dir
 dir D:\a\nicedishy-windows\nicedishy-windows
-dir D:\a\nicedishy-windows\nicedishy-windows\x86
-dir D:\a\nicedishy-windows\nicedishy-windows\x86\Release
+dir D:\a\nicedishy-windows\nicedishy-windows\NiceDishy
+dir D:\a\nicedishy-windows\nicedishy-windows\NiceDishy\x86\Release
 echo "Creating x86 zip"
-powershell Compress-7Zip -Path ([IO.Path]::GetFullPath((join-path (Get-Location) "..\x86\Release\NiceDishy.exe"))) -ArchiveFileName "NiceDishyX86.zip" -Format Zip
+powershell Compress-7Zip -Path ([IO.Path]::GetFullPath((join-path (Get-Location) "..\NiceDishy\x86\Release\NiceDishy.exe"))) -ArchiveFileName "NiceDishyX86.zip" -Format Zip
 echo "Creating x64 zip"
-powershell Compress-7Zip -Path ([IO.Path]::GetFullPath((join-path (Get-Location) "..\x64\Release\NiceDishy.exe"))) -ArchiveFileName "NiceDishyX64.zip" -Format Zip
+powershell Compress-7Zip -Path ([IO.Path]::GetFullPath((join-path (Get-Location) "..\NiceDishy\x64\Release\NiceDishy.exe"))) -ArchiveFileName "NiceDishyX64.zip" -Format Zip
 dir
 :exit
 popd
