ebd0d5eb03647351ca51a95f1bf1f01132b1f709
Marc Campbell <marc.e.campbell@gmail.com>
2022-03-09 21:04:15 -0800

    Path

--- deploy/ArtifactBuild.cmd
+++ deploy/ArtifactBuild.cmd
@@ -2,9 +2,9 @@
 pushd "%~dp0"
 powershell [IO.Path]::GetFullPath((join-path (Get-Location) "\x86\Release\NiceDishy.exe"))
 echo "Creating x86 zip"
-powershell Compress-7Zip -Path ([IO.Path]::GetFullPath((join-path (Get-Location) "\x86\Release\NiceDishy.exe"))) -ArchiveFileName "NiceDishyX86.zip" -Format Zip
+powershell Compress-7Zip -Path ([IO.Path]::GetFullPath((join-path (Get-Location) "..\x86\Release\NiceDishy.exe"))) -ArchiveFileName "NiceDishyX86.zip" -Format Zip
 echo "Creating x64 zip"
-powershell Compress-7Zip -Path ([IO.Path]::GetFullPath((join-path (Get-Location) "\x64\Release\NiceDishy.exe"))) -ArchiveFileName "NiceDishyX64.zip" -Format Zip
+powershell Compress-7Zip -Path ([IO.Path]::GetFullPath((join-path (Get-Location) "..\x64\Release\NiceDishy.exe"))) -ArchiveFileName "NiceDishyX64.zip" -Format Zip
 dir
 :exit
 popd
